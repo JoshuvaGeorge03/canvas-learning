8cbad5150124bdede7095114b982bc5a76e9ec69
Joshuva George <joshuva.m@zohocorp.com>
2023-07-05 08:53:22 +0530

    practiced how to add text in a canvas

--- utils.js
+++ utils.js
@@ -11,3 +11,19 @@ export function addListener(ele, type, cb) {
     ele.addEventListener(type, cb);
     return () => ele.removeEventListener(type, cb);
 }
+
+/*
+ 1) return new function
+ 2) when calling that function, it inturn calls all of the given function in right to left manner.
+ 3) facilitating passing of one function output to another function input.
+ 4) once every function called, it will return final output.
+*/
+export function compose(...fns) {
+    const copyOfFns = [...fns];
+    return function innerComposed(input) {
+        return copyOfFns.reduceRight((previousOutput, fn) => {
+            const outputOfFn = fn(previousOutput);
+            return outputOfFn;
+        }, input)
+    }
+}
